34df73fe1da628ea2f1425bceba62f8fcd600aa8
data_box <whaydigital@gmail.com>
2024-12-30 05:57:19 -0500

    changed footer

--- app/(preview)/page.tsx
+++ app/(preview)/page.tsx
@@ -158,7 +158,7 @@ export default function ChatWithFiles() {
             </CardTitle>
             <CardDescription className="text-base">
               Upload a PDF to generate an interactive quiz based on its content
-              using the <Link href="https://sdk.vercel.ai">AI SDK</Link> and{" "}
+              using the 
               <Link href="https://sdk.vercel.ai/providers/ai-sdk-providers/google-generative-ai">
                 Google&apos;s Gemini Pro - MVP
               </Link>
@@ -237,20 +237,20 @@ export default function ChatWithFiles() {
       >
         <NextLink
           target="_blank"
-          href="https://github.com/vercel-labs/ai-sdk-preview-pdf-support"
+          href="https://akselsoft.com"
           className="flex flex-row gap-2 items-center border px-2 py-1.5 rounded-md hover:bg-zinc-100 dark:border-zinc-800 dark:hover:bg-zinc-800"
         >
           <GitIcon />
-          View Source Code
+          Built by Akselsoft
         </NextLink>
 
         <NextLink
           target="_blank"
-          href="https://vercel.com/templates/next.js/ai-quiz-generator"
+          href="https://www.linkedin.com/company/103474436/admin/dashboard/"
           className="flex flex-row gap-2 items-center bg-zinc-900 px-2 py-1.5 rounded-md text-zinc-50 hover:bg-zinc-950 dark:bg-zinc-100 dark:text-zinc-900 dark:hover:bg-zinc-50"
         >
           <VercelIcon size={14} />
-          Deploy with Vercel
+          Contact Us on Linkedin
         </NextLink>
       </motion.div>
     </div>
